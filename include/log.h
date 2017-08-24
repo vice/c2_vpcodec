4d87a9b2fe3faef84882a7b21c34ec4efbabe479
Vicente Jimenez Aguilar <googuy@gmail.com>
2017-08-24 12:09:31 +0200

    Implement log levels

--- include/log.h
+++ include/log.h
@@ -1,12 +1,22 @@
-// TODO: Define log levels
+// Log Levels
+// 0 or undefined: Disable Logs
+// 1: Only Errors
+// 2: Errors and Infos
+// 3: Errors, Infos and Debugs
+// 4: All: Errors, Infos, Debugs and Verbose
+#define ALOGE(fmt, ...)
+#define ALOGI(fmt, ...)
+#define ALOGD(fmt, ...)
+#define ALOGV(fmt, ...)
 #if LOG_LEVEL
-#  define ALOGV(fmt, ...) fprintf(stderr, LOG_TAG " Verbose: " fmt "\n", __VA_ARGS__)
-#  define ALOGD(fmt, ...) fprintf(stderr, LOG_TAG " Debug: " fmt "\n", __VA_ARGS__)
-#  define ALOGI(fmt, ...) fprintf(stderr, LOG_TAG " Info: " fmt "\n", __VA_ARGS__)
 #  define ALOGE(fmt, ...) fprintf(stderr, LOG_TAG " Error: " fmt "\n", __VA_ARGS__)
-#else
-#  define ALOGV(fmt, ...)
-#  define ALOGD(fmt, ...)
-#  define ALOGI(fmt, ...)
-#  define ALOGE(fmt, ...)
+#endif
+#if LOG_LEVEL > 1
+#  define ALOGI(fmt, ...) fprintf(stderr, LOG_TAG " Info: " fmt "\n", __VA_ARGS__)
+#endif
+#if LOG_LEVEL > 2
+#  define ALOGD(fmt, ...) fprintf(stderr, LOG_TAG " Debug: " fmt "\n", __VA_ARGS__)
+#endif
+#if LOG_LEVEL > 3
+#  define ALOGV(fmt, ...) fprintf(stderr, LOG_TAG " Verbose: " fmt "\n", __VA_ARGS__)
 #endif
